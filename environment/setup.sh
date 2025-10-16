33b42b4994053fd5f069f92c574018891b16b279
Tom Chartrand <tom.chartrand@alleninstitute.org>
2025-10-16 00:38:11 +0000

    Added secrets.json, install_vscode, mcp.json; and changes in 5 other files

--- environment/setup.sh
+++ environment/setup.sh
@@ -3,6 +3,7 @@
 # link in vscode settings
 rm /root/capsule/code/.vscode/Machine/settings.json
 ln -s /root/vscode_machine_settings.json /root/capsule/code/.vscode/Machine/settings.json
+ln -s /root/mcp.json /root/capsule/code/.vscode/Machine/mcp.json
 
 # Cline MCP settings
 CLINE_DIR="$HOME/capsule/code/.vscode/User/globalStorage/saoudrizwan.claude-dev/settings"
@@ -19,4 +20,21 @@ if [ ! -f "$CLINE_CONFIG" ]; then
   }
 }
 EOF
+fi
+
+DIRECTORY=/root/.dotfiles
+# install dotfiles via git if $DOTFILES_REPO is set
+if test -n "$DOTFILES_REPO"; then
+  if [ -d "$DIRECTORY" ]; then
+    echo "updating dotfiles"
+    cd $DIRECTORY
+    git pull
+  else
+    echo "installing dotfiles"
+    git clone $DOTFILES_REPO $DIRECTORY --depth 1
+    cd $DIRECTORY
+  fi
+  # install script should be idempotent, fast, and ideally not overwrite user changes
+  bash install.sh
+  cd -
 fi
