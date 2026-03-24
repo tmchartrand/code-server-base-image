c999b634c01376148c0e0f9f6a49fd32bce9c68e
tmchartrand <tom.chartrand@alleninstitute.org>
2026-03-24 04:58:05 +0000

    remove mcp, update versions

--- environment/files/setup.sh
+++ environment/files/setup.sh
@@ -4,20 +4,6 @@
 rm /root/capsule/code/.vscode/Machine/settings.json
 ln -s /root/vscode_machine_settings.json /root/capsule/code/.vscode/Machine/settings.json
 
-VSCODE_DIR=/root/capsule/code/.vscode/User/
-VSCODE_CONFIG="$VSCODE_DIR/mcp.json"
-if [ ! -f "$VSCODE_CONFIG" ]; then
-  mkdir -p $VSCODE_DIR
-  cp /root/mcp.json "$VSCODE_CONFIG"
-fi
-# Cline MCP settings
-CLINE_DIR="$HOME/capsule/code/.vscode/User/globalStorage/saoudrizwan.claude-dev/settings"
-CLINE_CONFIG="$CLINE_DIR/cline_mcp_settings.json"
-
-if [ ! -f "$CLINE_CONFIG" ]; then
-  mkdir -p $CLINE_DIR
-  cp /root/cline_mcp_settings.json "$CLINE_CONFIG"
-fi
 
 DIRECTORY=/root/.dotfiles
 # install dotfiles via git if $DOTFILES_REPO is set
